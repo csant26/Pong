c528700e14753dc4b34fb44d4bd5fe59ac47f523
csant26 <shishantstha@gmail.com>
2026-01-29 14:32:01 +0545

    feat: added the movement for the paddle within defined boundaries

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ class PongGame:
     """Pong game"""
     def __init__(self):
         self.initialize_screen()
+        self.game_on = True
         self.paddles: list[paddle.Paddle] = []
         self.initialize_paddles()
-        self.screen.update()
-        self.screen.exitonclick()
+        self.game_loop()
+
 
     def initialize_screen(self):
         """Initialize Pong screen"""
@@ -27,5 +28,29 @@ class PongGame:
         paddle2 = paddle.Paddle()
         paddle1.goto(x=cons.FIRST_PADDLE_XCOOR,y=cons.FIRST_PADDLE_YCOOR)
         paddle2.goto(x=cons.SECOND_PADDLE_XCOOR,y=cons.SECOND_PADDLE_YCOOR)
+        self.paddles.extend([paddle1,paddle2])
+        self.initialize_paddle_motion()
+
+    def initialize_paddle_motion(self):
+        """Initialize paddle motion through key listen"""
+        self.screen.listen()
+        self.screen.onkey(self.paddles[0].move_paddle_up,"Up")
+        self.screen.onkey(self.paddles[0].move_paddle_down,"Down")
+        self.screen.onkey(self.paddles[1].move_paddle_up,"w")
+        self.screen.onkey(self.paddles[1].move_paddle_down,"s")
+
+    def game_loop(self):
+        """Runs Pong on loop"""
+        if not self.game_on:
+            return
+        self.screen.update()
+        self.screen.ontimer(self.game_loop,16)
+
+    def run(self):
+        """Run Pong"""
+        self.game_loop()
+        self.screen.mainloop()
+
 
 pong_game = PongGame()
+pong_game.run()
--- paddle.py
+++ paddle.py
@@ -1,6 +1,7 @@
 """Setting up Paddle"""
 import turtle
 import constants as cons
+
 class Paddle(turtle.Turtle):
     """Paddle"""
     def __init__(self):
@@ -8,4 +9,20 @@ class Paddle(turtle.Turtle):
         self.shape(cons.PADDLE_SHAPE)
         self.color(cons.PADDLE_COLOUR)
         self.penup()
-        self.shapesize(stretch_wid=5,stretch_len=1)
+        self.shapesize(stretch_wid=cons.PADDLE_STRETCH_WIDTH,stretch_len=1)
+
+    def move_paddle_up(self):
+        """Moves paddle up"""
+        new_y = self.ycor() + cons.PADDLE_PACE
+        if new_y > cons.PADDLE_MAX_YCOOR:
+            new_y = cons.PADDLE_MAX_YCOOR
+        self.sety(new_y)
+
+    def move_paddle_down(self):
+        """Moves paddle down"""
+        new_y = self.ycor() - cons.PADDLE_PACE
+        if new_y < cons.PADDLE_MIN_YCOOR:
+            new_y = cons.PADDLE_MIN_YCOOR
+        self.sety(new_y)
+
+
